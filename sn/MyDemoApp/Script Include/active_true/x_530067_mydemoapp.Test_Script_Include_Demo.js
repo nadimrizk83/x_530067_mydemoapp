6a1db42f9aa0619800c8f07e2377435e9e6a7f9d
System Administrator <admin@example.com>
2020-09-02 09:58:04 -0400

    Changes by 'System Administrator' on 'MyDemoApp - 1.0.11' -- Build #1

---
+++ sn/MyDemoApp/Script Include/active_true/x_530067_mydemoapp.Test_Script_Include_Demo.js
@@ -0,0 +1,28 @@
+/* 
+ * Application : MyDemoApp
+ * ClassName   : sys_script_include
+ * Created On  : 2020-09-02 13:47:03
+ * Created By  : admin
+ * Updated On  : 2020-09-02 13:47:11
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=177cc0c91b03141061ef31d7dc4bcb58
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_530067_mydemoapp.module:sys_script_include
+ */
+var Test_Script_Include_Demo = Class.create();
+Test_Script_Include_Demo.prototype = /** @lends x_530067_mydemoapp.module:sys_script_include.Test_Script_Include_Demo.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'Test_Script_Include_Demo'
+};
